a5928d0af14f533483dc58f5e618fe5cc2c78a43
Jair Alayon <jair.atl.95@gmail.com>
2020-11-30 22:52:28 -0600

    Adding FileService and its test in order to persist the profile images by users

---
+++ src/test/java/com/alayon/hoaxify/utils/FileServiceTest.java
@@ -0,0 +1,54 @@
+package com.alayon.hoaxify.utils;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.alayon.hoaxify.config.AppConfiguration;
+import com.alayon.hoaxify.file.FileService;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+public class FileServiceTest {
+
+	FileService fileService;
+
+	AppConfiguration appConfiguration;
+
+	@Before
+	public void init() {
+		appConfiguration = new AppConfiguration();
+		appConfiguration.setUploadPath("uploads-test");
+
+		fileService = new FileService(appConfiguration);
+
+		new File(appConfiguration.getUploadPath()).mkdir();
+		new File(appConfiguration.getFullProfileImagePath()).mkdir();
+		new File(appConfiguration.getFullAttachmentPath()).mkdir();
+	}
+
+	@After
+	public void cleanup() throws IOException {
+		FileUtils.cleanDirectory(new File(appConfiguration.getFullProfileImagePath()));
+		FileUtils.cleanDirectory(new File(appConfiguration.getFullAttachmentPath()));
+	}
+
+	@Test
+	public void detectType_whenPngFileProvided_returnsImagePng() throws IOException {
+		final ClassPathResource resourceFile = new ClassPathResource("test-png.png");
+		final byte[] fileArr = FileUtils.readFileToByteArray(resourceFile.getFile());
+		final String fileType = fileService.detectType(fileArr);
+		assertThat(fileType).isEqualToIgnoringCase("image/png");
+	}
+
+}
